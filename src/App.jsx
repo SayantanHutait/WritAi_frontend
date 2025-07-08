e22a572c110f8a3202fccc6d6765d33447a5741c
SayantanHutait <sayantan344hutait@gmail.com>
2025-07-09 01:20:02 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Chat from './Chat'
 import Login from './Auth/Login'
